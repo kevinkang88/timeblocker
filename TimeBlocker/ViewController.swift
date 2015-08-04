7e04ddf019441a37c8282e1e7589ba0d3f6b0538
dong yoon kang <yoon@dongs-MacBook-Pro.local>
2015-08-03 20:58:49 -0700

    mvp done

--- TimeBlocker/ViewController.swift
+++ TimeBlocker/ViewController.swift
@@ -9,18 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var count = 0
+    
+    func updateTime(){
+        count++
+        timeLabel.text = "\(count)"
+        
+    }
 
+    @IBOutlet var timeLabel: UILabel!
+    
     @IBAction func pauseTapped(sender: AnyObject) {
+        timer.invalidate()
     }
     
     @IBAction func playTapped(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
     }
     
     @IBAction func stopTapped(sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        timeLabel.text = "0"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         
     }
 
